96b40531ac8005321092b147b6ba5d7f238124ef
Hle14 <HansLe518@gmail.com>
2013-12-07 10:33:00 -0500

    restaurant schema added

--- models/restaurants.js
+++ models/restaurants.js
@@ -3,15 +3,16 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/Restaurants');
 //var dbConnection = mongoose.createConnection('localhost:27017','SearchMeDB'); //addition
 
+/*
 var validator = function(value,)
 {
 	User.findOne({})
-};
+};*/
 
 var restaurant = new mongoose.Schema({
 	name: String,
 	address: String,
-	dishes: Array
+	dishes: [{name: String,levels:{type:Number, min:0}}]
 });
 
 //module.exports = dbConnection.model('UserModel',userSchema);
